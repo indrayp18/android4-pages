598790ed5d8c0c34d3dc4d7f505abda7cf44e98d
borodan666 <dianpratama2@gmail.com>
2019-09-21 11:51:12 +0800

    2 pages commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'firstpage', loadChildren: './pages/firstpage/firstpage.module#FirstpagePageModule' },
+  { path: 'secondpage', loadChildren: './pages/secondpage/secondpage.module#SecondpagePageModule' },
 ];
 
 @NgModule({
