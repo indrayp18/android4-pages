107d01008779e55da635050c1d54f97f6b826454
borodan666 <dianpratama2@gmail.com>
2019-09-23 14:00:23 +0800

    firstpage-menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'firstpage', loadChildren: './pages/firstpage/firstpage.module#FirstpagePageModule' },
-  { path: 'secondpage', loadChildren: './pages/secondpage/secondpage.module#SecondpagePageModule' },
-  { path: 'thirdpage', loadChildren: './pages/thirdpage/thirdpage.module#ThirdpagePageModule' },
-  { path: 'fourthpage', loadChildren: './pages/fourthpage/fourthpage.module#FourthpagePageModule' },
+  { path: '', loadChildren: './pages/menu/menu.module#MenuPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/pages/menu/menu.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterEvent, } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+})
+export class MenuPage implements OnInit {
+
+  pages = [
+    {
+      title: 'Second Page',
+      url: '/menu/second'
+    }
+  ];
+
+  selectedPath= '';
+  
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.selectedPath = event.url;
+    })
+   }
+
+  ngOnInit() {
+  }
+
+}
